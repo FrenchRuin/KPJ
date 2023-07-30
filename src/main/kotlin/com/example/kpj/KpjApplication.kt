05ff60e89a14c853aa4ef8de8b5cb0913fa13904
이재찬 <toxic023@naver.com>
2023-07-30 22:00:14 +0900

    kotlin Spring Project

---
+++ src/main/kotlin/com/example/kpj/KpjApplication.kt
@@ -0,0 +1,11 @@
+package com.example.kpj
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KpjApplication
+
+fun main(args: Array<String>) {
+    runApplication<KpjApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/kpj/KpjApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.kpj
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KpjApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
